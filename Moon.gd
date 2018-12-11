0a67201f5fd6eb996e8296a1e1703f919639b75c
Garrett <31374168+JellyKisses@users.noreply.github.com>
2018-12-11 09:01:01 -0500

    Update Moon.gd

--- Moon.gd
+++ Moon.gd
@@ -1,13 +1,11 @@
 extends AnimatedSprite
 
-
 var parPos = Vector2()
 var angle = 0.0
 var dist = 100.0
 var vel = 100.0
 
 func _ready():
-	#parPos = get_parent().get_parent().transform.origin
 	match get_meta("role"):
 		1:
 			frame = 1
@@ -29,14 +27,9 @@ func _ready():
 			modulate.r = 0.3
 			modulate.g = 0.3
 			modulate.b = 1.0
-	#var frames = SpriteFrames()
-	
-	
 	pass
 
 func _process(delta):
-	#vel += (vel / 10.0) * delta
-	#dist -= delta * 5.0
 	vel = 35000 / dist
 	angle = fmod(angle + (delta * vel), 360.0)
 	
@@ -44,7 +37,6 @@ func _process(delta):
 	global_position.x = cos(r) * dist - sin(r) * dist + parPos.x
 	global_position.y = sin(r) * dist + cos(r) * dist + parPos.y
 	global_rotation = parPos.angle_to_point(global_position)
-	
 	pass
 
 func _on_Moon_animation_finished():
